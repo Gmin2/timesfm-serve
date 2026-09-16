d5525f0cf0e0208f79fcd51966b59ad1a0f01472
Gmin2 <mintugogoi567@gmail.com>
2026-09-16 16:45:26 +0530

    drop an unused import

--- iex/calibrate.py
+++ iex/calibrate.py
@@ -9,7 +9,7 @@ from that quantile on days whose outcome was already known at the cutoff.
 import numpy as np
 import pandas as pd
 
-from iex.backtest import BLOCKS, QUANTILE_LEVELS
+from iex.backtest import QUANTILE_LEVELS
 
 COLUMNS = [f"q{int(level * 100)}" for level in QUANTILE_LEVELS]
 MIN_CALIBRATION_DAYS = 30
